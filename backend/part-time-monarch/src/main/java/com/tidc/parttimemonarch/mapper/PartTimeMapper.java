55fcfd8718bd1ae4fcee4bbf19f057284aa67713
Sersph <1033278494@qq.com>
2018-10-30 22:43:31 +0800

    添加对 PartTime 操作的mapper

--- backend/part-time-monarch/src/main/java/com/tidc/parttimemonarch/mapper/PartTimeMapper.java
+++ backend/part-time-monarch/src/main/java/com/tidc/parttimemonarch/mapper/PartTimeMapper.java
@@ -33,16 +33,17 @@ public interface PartTimeMapper {
     @Select(value = "select * from settlement_type")
     List<PartTimeInfo> settlementTypes();
 
-    @Select(value = "")     //查询所有数据
+    @Select(value = "select * from part_time")
     List<PartTime> selectAllPartTime();
 
-//     @Insert("INSERT INTO user_info(name, user_id, role_id, email, city_id, created_at, updated_at, last_sign_in_at) " +
-//            "VALUES (#{name}, #{userId}, #{roleId}, #{email}, #{cityId}, #{createdAt}, #{updatedAt}, #{lastSignInAt})")
-
-
-    @Insert(value = "")     //插入一条数据
+    @Insert(value = "INSERT INTO part_time(name, province_id, city_id, area_id, detail_address, recruitment_cont, price, " +
+            "calculation_type_id, settlement_type_id, user_id, job_description, contact_people, contact_phone, working_date_start, " +
+            "working_date_end, working_time_week, working_time, price_description, part_time_type_id, part_time_species_id)" +
+            "VALUES (#{name}, #{province_id}, #{city_id}, #{area_id}, #{detail_address}, #{recruitment_cont}, #{price}," +
+            "#{calculation_type_id},#{settlement_type_id},#{user_id},#{job_description},#{contact_people},#{contact_phone},#{working_date_start}," +
+            "#{working_date_end}, #{working_time_week}, #{working_time}, #{price_description}, #{part_time_type_id}, #{part_time_species_id}")
     int addPartTime(PartTime partTime);
 
-    @Delete(value = "")     //通过id删除一条数据
+    @Delete(value = "delete from part_time where id = #{id}")     //通过id删除一条数据
     int deletePartTimeById(@Param(value = "id") int id);
 }
